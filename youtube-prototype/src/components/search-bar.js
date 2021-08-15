5c6e2f8f1ddfdca91077858cc5a60b58fb4a6a34
sureshraxz <sureshraxz@gmail.comm>
2021-08-15 06:20:56 +0530

    search bar functionality - functional vs class component, state, event handlers, exports, ...

--- youtube-prototype/src/components/search-bar.js
+++ youtube-prototype/src/components/search-bar.js
@@ -1,1 +1,28 @@
-import React from 'react';
+import React,{Component} from 'react';
+
+class SearchBar extends Component{
+    //inbuild fns - constructor(provided by class es6), super(oops), state(react) 
+    constructor(props){
+        super(props);
+
+        this.state = {search_term:''};
+    }
+    //render element - once we setState to a component element it starts rerendering
+    render(){
+        return (
+            <input 
+            value={this.state.search_term}
+            onChange={event => this.onInputChange(event.target.value)} />
+        );
+    }
+    //input event handler function
+    onInputChange(search_term){        
+        this.setState({search_term:search_term});     //setting state
+        this.props.onSearchBarChange(search_term);      //passing data to call back fn - which is called in index.js
+    }
+}
+
+export default SearchBar;
+
+
+
--- youtube-prototype/src/index.js
+++ youtube-prototype/src/index.js
@@ -1,1 +1,26 @@
-const API_KEY = ''; //paste your key
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search-bar';
+import YTSearchApi from 'youtube-api-search';
+
+const API_KEY = 'AIzaSyAGSqpH1_fHJFCgRNKY-l5qPloMQdcNZ9w'; //paste your key
+
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.videoApiCall('rdj');
+    }
+    render(){
+        return (
+            <SearchBar onSearchBarChange={term => this.videoApiCall(term)} />
+        );
+    }
+    //youtube search api 
+    videoApiCall(search_term){
+        YTSearchApi({key:API_KEY, term:search_term}, (videos_data) =>{
+            console.log(videos_data);
+        });
+    }
+}
+
+ReactDOM.render(<App/>,document.querySelector('.index-container'));
